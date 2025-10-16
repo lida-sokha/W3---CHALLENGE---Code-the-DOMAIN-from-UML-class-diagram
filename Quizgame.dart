9f26f252db11a9f306e58e5c56cd8532ea6c57cc
lida-sokha <sokhalida1234@gmail.com>
2025-10-17 00:22:33 +0700

    finish

--- Quizgame.dart
+++ Quizgame.dart
@@ -1,11 +1,11 @@
 class Quiz {
   List<Question> questions = [];
-  List<Answer> addAnswer;
-  Quiz(this.addAnswer, this.questions);
+  List<Answer> Answers = [];
+  Quiz({required this.questions});
 
   int getScore() {
     int score = 0;
-    for (var answer in addAnswer) {
+    for (var answer in Answers) {
       if (answer.isGoodAnswer()) {
         score++;
       }
@@ -36,4 +36,28 @@ class Answer {
   bool isGoodAnswer() {
     return answerChoice == question.goodChoice;
   }
+}
+
+main() {
+  // Quiz country = Quiz(["what is the best country?"], []);
+  Question q1 = Question(
+    title: "country",
+    choices: ["England", "cambodia"],
+    goodChoice: "Cambodia",
+  );
+  Question q2 = Question(
+    title: "country",
+    choices: ["Maxico", "USA"],
+    goodChoice: "USA",
+  );
+
+  var quiz = Quiz(questions: [q1, q2]);
+  Answer player1a1 = Answer("Cambodia", q1);
+  Answer player1a2 = Answer("USA", q2);
+
+  quiz.Answers.add(player1a1);
+  quiz.Answers.add(player1a2);
+  
+  print("${player1a2.isGoodAnswer()} && ${player1a1.isGoodAnswer()}");
+  print("\nscore: ${quiz.getScore()}/ ${quiz.questions.length}");
 }
